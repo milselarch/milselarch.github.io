9af21d9fb32306a3cd9ad45df6cf9c4a783c019c
milselarch <milselarch@gmail.com>
2025-04-01 22:13:05 +0800

    Updates

--- src/components/icons/logo.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Logo from '../../images/personal-logo.svg'; // Adjust the path to your SVG file
-
-const IconLogo = () => (
-  <img src={Logo} alt="Logo" id="logo"/>
-);
-
-export default IconLogo;
