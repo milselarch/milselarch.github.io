a1036acb829a8b587d876b1a348ecd12a40a60ba
milselarch <milselarch@gmail.com>
2025-10-26 22:49:46 +0800

    fix: check if window is defined

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,7 +2,8 @@ export const ALL_BLOG_POSTS_MESSAGE = "All Blog Posts"
 export const BLOG = "blog"
 
 export const GET_ALLOW_EDGY_BLOG_POSTS = () => {
+  if (typeof window === 'undefined') return false;
   return [
     'localhost', 'milselarch.com'
-  ].includes(window.location.hostname)
+  ].includes(window.location.hostname);
 }
